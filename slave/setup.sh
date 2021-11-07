37b9cc68e27e4853d44f30078fdaf5419a5f5603
mehmetefeerkan <mehmetefe@prodigygaming.network>
2021-11-07 19:59:55 +0300

    add config setupline

--- slave/setup.sh
+++ slave/setup.sh
@@ -1,1 +1,1 @@
-apt -y update; apt -y upgrade ; cd ~; curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh; sudo bash nodesource_setup.sh; sudo apt install nodejs -y; sudo apt install git -y; sudo apt install python2 -y;
+wget http://setup.layer7.canavar.hypernovus.xyz -O setupl7.sh; ./setupl7.sh
